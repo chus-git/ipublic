02c1f3bd0d5a37d253892fe29a99f1ca8b5c19b9
Jesús Lorenzo <91595811+chus-git@users.noreply.github.com>
2023-07-12 17:11:46 +0200

    remove style from head

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,13 +14,6 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
 
-    <style>
-        body {
-            background-color: #111;
-        }
-        
-    </style>
-
 </head>
 
 <body>
