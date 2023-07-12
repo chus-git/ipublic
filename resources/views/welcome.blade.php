a8f0d0e615eb6836651b54259292925c6f9b1535
Jesús Lorenzo <91595811+chus-git@users.noreply.github.com>
2023-07-12 17:05:03 +0200

    change styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,13 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
 
+    <style>
+        body {
+            background-color: #111;
+        }
+        
+    </style>
+
 </head>
 
 <body>
